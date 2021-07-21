c7215de3e301221f70917cee30535199539392cf
ajayraokunjoor <ajayraokunjoor@hotmail.com>
2021-07-21 15:11:02 +0530

    First Commit from Mac M1 using VSCode

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello Ajay Rao Kunjoor";
+            Console.WriteLine(message);
+            Console.WriteLine("Rakshitha Ajay Rao");
+        }
+    }
+}
